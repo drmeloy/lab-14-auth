6a621e1cbbda7341e87cd3b99bad480ee167615a
drmeloy <drmeloy@gmail.com>
2019-12-19 14:42:35 -0700

    sign up route and test done

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,8 +4,9 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const User = require('../lib/models/User');
 
-describe('app routes', () => {
+describe('auth routes', () => {
   beforeAll(() => {
     connect();
   });
@@ -16,5 +17,23 @@ describe('app routes', () => {
 
   afterAll(() => {
     return mongoose.connection.close();
+  });
+
+  it('creates a new user', () => {
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        username: 'drmeloy',
+        email: 'drmeloy@gmail.com',
+        password: 'hype'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          username: 'drmeloy',
+          email: 'drmeloy@gmail.com',
+          __v: 0
+        });
+      });
   });
 });
